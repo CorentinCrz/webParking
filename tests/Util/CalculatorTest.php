9f564597f098305704b3dd9221a8934a0a8a45cc
CorentinCrz <ccroiz@live.fr>
2019-05-15 20:50:05 +0200

    php-cs-fixer run

--- tests/Util/CalculatorTest.php
+++ tests/Util/CalculatorTest.php
@@ -13,6 +13,6 @@ class CalculatorTest extends TestCase
         $result = $calculator->add(30, 12);
 
         // assert that your calculator added the numbers correctly!
-        $this->assertEquals(42, $result);
+        $this->assertSame(42, $result);
     }
 }
