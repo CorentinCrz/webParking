28e92db74cfdb269f82cea789c7d4e00ed492359
CorentinCrz <ccroiz@live.fr>
2019-05-18 21:51:58 +0200

    fix

--- tests/Controller/AccountControllerTest.php
+++ tests/Controller/AccountControllerTest.php
@@ -3,9 +3,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class AccountControllerTest extends WebTestCase
 {
